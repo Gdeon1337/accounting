80602367737c58b35785cc776a158edf9b4495d8
Gdeon1337 <gdeom1337@gmail.com>
2020-01-30 16:27:16 +0400

    ref fallbackcontroller

--- lib/accounting_web/controllers/fallback_controller.ex
+++ lib/accounting_web/controllers/fallback_controller.ex
@@ -1,22 +1,23 @@
 defmodule AccountingWeb.FallbackController do
     use AccountingWeb, :controller
+    alias AccountingWeb.ErrorView, as: ErrorView 
 
     def call(conn, {:error, :incorrect_data}) do
         conn
         |> put_status(415)
-        |> json(%{status: "Incorrect data"})
+        |> render(ErrorView, "error.json", %{status: "Incorrect data"})
     end
 
     def call(conn, {:error, :no_connection_redis}) do
         conn
         |> put_status(500)
-        |> json(%{status: "Redis disconnected"})
+        |> render(ErrorView, "error.json", %{status: "Redis disconnected"})
     end
 
     def call(conn, {:error, _message}) do
         conn
         |> put_status(500)
-        |> json(%{status: "Unknown error"})
+        |> render(ErrorView, "error.json", %{status: "Unknown error"})
     end
     
 end
--- lib/accounting_web/views/error_view.ex
+++ lib/accounting_web/views/error_view.ex
@@ -10,6 +10,11 @@ defmodule AccountingWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
+
+  def render("error.json", %{status: status}) do
+    %{status: status}
+  end
+
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
